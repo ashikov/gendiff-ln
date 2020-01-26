b026d7317efd5ad84ef38238a1a48dc89c7e21bb
ashikov <rmn.shkv@gmail.com>
2020-01-26 16:17:41 +0700

    implemented cases object

--- src/__tests__/difference.test.js
+++ src/__tests__/difference.test.js
@@ -1,22 +1,23 @@
 import fs from 'fs';
 import genDiff from '../genDiff';
 
+const cases = {
+  json: {
+    path1: `${__dirname}/../__fixtures__/json/before1.json`,
+    path2: `${__dirname}/../__fixtures__/json/after1.json`,
+    expected: fs.readFileSync(`${__dirname}/../__fixtures__/expected1`, 'utf-8'),
+  },
+  yaml: {
+    path1: `${__dirname}/../__fixtures__/yaml/before1.yml`,
+    path2: `${__dirname}/../__fixtures__/yaml/after1.yml`,
+    expected: fs.readFileSync(`${__dirname}/../__fixtures__/expected1`, 'utf-8'),
+  },
+};
+
 test('genDiff -> JSON', () => {
-  const format = 'json';
-  const path1 = `${__dirname}/../__fixtures__/json/before1.json`;
-  const path2 = `${__dirname}/../__fixtures__/json/after1.json`;
-
-  const expected = fs.readFileSync(`${__dirname}/../__fixtures__/expected1`, 'utf-8');
-
-  expect(genDiff(format, path1, path2)).toEqual(expected);
+  expect(genDiff(cases.json.path1, cases.json.path2)).toEqual(cases.json.expected);
 });
 
 test('genDiff -> YAML', () => {
-  const format = 'yaml';
-  const path1 = `${__dirname}/../__fixtures__/yaml/before1.yml`;
-  const path2 = `${__dirname}/../__fixtures__/yaml/after1.yml`;
-
-  const expected = fs.readFileSync(`${__dirname}/../__fixtures__/expected1`, 'utf-8');
-
-  expect(genDiff(format, path1, path2)).toEqual(expected);
+  expect(genDiff(cases.yaml.path1, cases.yaml.path2)).toEqual(cases.yaml.expected);
 });
