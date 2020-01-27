2ae3e534613b36246c6420aaf62454b1fa404be5
ashikov <rmn.shkv@gmail.com>
2020-01-27 16:38:16 +0700

    tests refactoring

--- src/__tests__/difference.test.js
+++ src/__tests__/difference.test.js
@@ -1,23 +1,20 @@
 import fs from 'fs';
 import genDiff from '../genDiff';
 
-const cases = {
-  json: {
-    path1: `${__dirname}/../__fixtures__/json/before1.json`,
-    path2: `${__dirname}/../__fixtures__/json/after1.json`,
-    expected: fs.readFileSync(`${__dirname}/../__fixtures__/expected1`, 'utf-8'),
-  },
-  yaml: {
-    path1: `${__dirname}/../__fixtures__/yaml/before1.yml`,
-    path2: `${__dirname}/../__fixtures__/yaml/after1.yml`,
-    expected: fs.readFileSync(`${__dirname}/../__fixtures__/expected1`, 'utf-8'),
-  },
-};
+describe('genDiffTest', () => {
+  it('JSON', () => {
+    const jsonFile1 = `${__dirname}/../__fixtures__/json/before1.json`;
+    const jsonFile2 = `${__dirname}/../__fixtures__/json/after1.json`;
+    const jsonResult1 = fs.readFileSync(`${__dirname}/../__fixtures__/expected1`, 'utf-8');
 
-test('genDiff -> JSON', () => {
-  expect(genDiff(cases.json.path1, cases.json.path2)).toEqual(cases.json.expected);
-});
+    expect(genDiff(jsonFile1, jsonFile2)).toEqual(jsonResult1);
+  });
 
-test('genDiff -> YAML', () => {
-  expect(genDiff(cases.yaml.path1, cases.yaml.path2)).toEqual(cases.yaml.expected);
+  it('YAML', () => {
+    const yamlFile1 = `${__dirname}/../__fixtures__/yaml/before1.yml`;
+    const yamlFile2 = `${__dirname}/../__fixtures__/yaml/after1.yml`;
+    const yamlResult1 = fs.readFileSync(`${__dirname}/../__fixtures__/expected1`, 'utf-8');
+
+    expect(genDiff(yamlFile1, yamlFile2)).toEqual(yamlResult1);
+  });
 });
