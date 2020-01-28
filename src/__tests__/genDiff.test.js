53495d99346948a4943478e5e1d70f2beeafcad4
ashikov <rmn.shkv@gmail.com>
2020-01-28 12:51:04 +0700

    refactoring tests

--- src/__tests__/genDiff.test.js
+++ src/__tests__/genDiff.test.js
@@ -1,16 +1,14 @@
 import fs from 'fs';
 import genDiff from '..';
 
-const pathPrefix = `${__dirname}/../__fixtures__/`;
+const prefix = `${__dirname}/../__fixtures__`;
 
 describe.each([
-  [`${pathPrefix}json/before1.json`, `${pathPrefix}json/after1.json`],
-  [`${pathPrefix}yaml/before1.yml`, `${pathPrefix}yaml/after1.yml`],
-  [`${pathPrefix}ini/before1.ini`, `${pathPrefix}ini/after1.ini`],
+  [`${prefix}/before.json`, `${prefix}/after.json`],
 ])('testGenDiff', (file1, file2) => {
-  const result1 = fs.readFileSync(`${__dirname}/../__fixtures__/expected1`, 'utf-8');
+  const result = fs.readFileSync(`${prefix}/result.txt`, 'utf-8');
 
-  test(`returns\n${result1}\n`, () => {
-    expect(genDiff(file1, file2)).toEqual(result1);
+  test('returns diff\n', () => {
+    expect(genDiff(file1, file2)).toEqual(result);
   });
 });
