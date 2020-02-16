4447bf266be7517ce021a3b5c9fe5258c64dc2a1
ashikov <rmn.shkv@gmail.com>
2020-02-16 19:02:54 +0700

    refactored tests

---
+++ __tests__/genDiff.test.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import genDiff from '../src/index';
+
+const prefix = `${__dirname}/../__fixtures__`;
+const getFullPath = (format, name) => `${prefix}/${name}.${format}`;
+
+describe.each([
+  ['json'],
+  ['yml'],
+  ['ini'],
+])('diff', (format) => {
+  const pathToFile1 = getFullPath(format, 'before');
+  const pathToFile2 = getFullPath(format, 'after');
+
+  const result = fs.readFileSync(`${prefix}/result.txt`, 'utf-8');
+  const resultPlain = fs.readFileSync(`${prefix}/resultPlain.txt`, 'utf-8');
+  const resultJSON = fs.readFileSync(`${prefix}/resultJSON.txt`, 'utf-8');
+
+  test(`${format} to default`, () => {
+    expect(genDiff(pathToFile1, pathToFile2)).toEqual(result);
+  });
+
+  test(`${format} to plain`, () => {
+    expect(genDiff(pathToFile1, pathToFile2, 'plain')).toEqual(resultPlain);
+  });
+
+  test(`${format} to JSON`, () => {
+    expect(genDiff(pathToFile1, pathToFile2, 'json')).toEqual(resultJSON);
+  });
+});
