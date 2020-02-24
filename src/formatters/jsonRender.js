6059173c536ecdc0f73ed0f7ff09c92934e62435
ashikov <rmn.shkv@gmail.com>
2020-02-24 11:42:16 +0700

    add stringify function

--- src/formatters/jsonRender.js
+++ src/formatters/jsonRender.js
@@ -1,11 +1,13 @@
-const jsonRender = (ast) => {
-  const result = JSON.stringify(ast, (key, value) => {
-    if (typeof value === 'number') {
-      return String(value);
-    }
+const stringify = (value) => {
+  if (typeof value === 'number') {
+    return String(value);
+  }
 
-    return value;
-  }, 2);
+  return value;
+};
+
+const jsonRender = (ast) => {
+  const result = JSON.stringify(ast, (key, value) => stringify(value), 2);
 
   return result;
 };
