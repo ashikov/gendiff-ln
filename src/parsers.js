6221c5cfba1dc3f3043593bc3ed7468723136d5e
ashikov <rmn.shkv@gmail.com>
2020-03-03 11:08:28 +0700

    implementation processing numeric values in iniParser

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,33 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
+import { isNaN } from 'lodash';
+
+const isNumeric = (value) => {
+  if (typeof value === 'boolean') {
+    return false;
+  }
+
+  const convertedValue = +value;
+
+  if (isNaN(convertedValue)) {
+    return false;
+  }
+
+  return true;
+};
+
+const iniParser = (data) => {
+  const jsonString = JSON.stringify(ini.parse(data));
+
+  const parsed = JSON.parse(jsonString, (_k, v) => (isNumeric(v) ? Number(v) : v));
+
+  return parsed;
+};
 
 const parsers = {
   json: JSON.parse,
   yml: yaml.safeLoad,
-  ini: ini.parse,
+  ini: iniParser,
 };
 
 export default (format, data) => parsers[format](data);
